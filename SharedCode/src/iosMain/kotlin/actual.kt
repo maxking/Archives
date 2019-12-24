f5a0449dde48b19318e737d66fe75b373e1a226f
Abhilash Raj <maxking@asynchronous.in>
2019-12-24 14:19:58 -0800

    Initial commit with a working Hello World!
    Signed-off-by: Abhilash Raj <maxking@asynchronous.in>

---
+++ SharedCode/src/iosMain/kotlin/actual.kt
@@ -0,0 +1,8 @@
+package com.jetbrains.handson.mpp.mobile
+
+import platform.UIKit.UIDevice
+
+
+actual fun platformName(): String {
+    return UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
